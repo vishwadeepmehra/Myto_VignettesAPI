8fe8bef7656d7f2c4e08620b840019bfc4f65663
sum-curn <abhimanyusingh01322000@gmail.com>
2025-11-21 10:42:11 +0530

    Database connection string changes.

--- Myto_VignettesAPI/Program.cs
+++ Myto_VignettesAPI/Program.cs
@@ -91,12 +91,9 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
